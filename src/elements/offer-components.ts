24ddc6aa3e34be69e6c584ab3efe0ca0ba8d75bd
mishevski-k <mishevski0800@gmail.com>
2022-01-25 03:38:10 +0100

    created and build offer card structure.

--- src/elements/offer-components.ts
+++ src/elements/offer-components.ts
@@ -25,3 +25,61 @@ class OAPOffersWrapper extends LitElement
         `;
     }
 }
+
+@customElement("oap-offer-card")
+class OAPOfferCard extends LitElement
+{
+    static styles = css`
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+        *
+        {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+    `;
+
+    @property()
+    offerImage = "https://intro-web-dev.media-ed-online.com/topic-12/img/hw-icon-placeholdergen.png";
+    @property()
+    offerRewardValue = "0";
+    @property()
+    offerTitle = "No Title Found";
+    @property()
+    offerDescription = "No Description is present";
+    @property()
+    offerButtonText = "";
+    @property()
+    OfferButtonURL = "";
+
+    ifOfferHasButton()
+    {
+        if(this.offerButtonText != "" && this.OfferButtonURL != "")
+        {
+            return html`<a class="oap-offer-link" href="${this.OfferButtonURL}">${this.offerButtonText}</a>`;
+        }
+        else
+        {
+            return html``;
+        }
+    }
+
+    render()
+    {
+        return html`
+            <div class="oap-offer-banner">
+                <img src="${this.offerImage}">
+                <div class="oap-offer-reward">
+                    <h1>${this.offerRewardValue}</h1>
+                </div>
+            </div>
+            <div class="oap-offer-details">
+                <h1 class="oap-offer-title">${this.offerTitle}</h1>
+                <h2 class="oap-offer-description">${this.offerDescription}</h2>
+                ${this.ifOfferHasButton()}
+            </div>
+        `;
+    }
+
+}
