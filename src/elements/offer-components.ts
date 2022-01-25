3835f26b2ec22667c1dca492591a745f0b2fe600
mishevski-k <mishevski0800@gmail.com>
2022-01-25 03:26:13 +0100

    Created and build wrapper for offers

--- src/elements/offer-components.ts
+++ src/elements/offer-components.ts
@@ -1,2 +1,27 @@
 import {LitElement, html, css} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
+
+@customElement('oap-offers-wrapper')
+class OAPOffersWrapper extends LitElement
+{
+    static styles = css`
+        :host
+        {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(22rem, 1fr));
+            gap: 1.5rem;
+            align-items: center;
+            width: 100%;
+            padding: 0.2rem;
+            margin: 0;
+            box-sizing: border-box;
+        }
+    `;
+
+    render()
+    {
+        return html`
+            <slot></slot>
+        `;
+    }
+}
